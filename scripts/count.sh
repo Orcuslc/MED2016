1c10ff6e278ee601b441662ae0bce2ca6d3d7a38
C.L <chuanlu@outlook.com>
2016-08-10 07:36:15 +0800

    Add a script to count the result

---
+++ scripts/count.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash                                                        
+cd training-1/txt/ && ls -l|wc -l >> ../../count.txt                      
+cd ../../training-2/txt/ && ls -l|wc -l >> ../../count.txt                 
+cd ../../test-1/txt/ && ls -l|wc -l >> ../../count.txt                     
+cd ../../test-2/txt/ && ls -l|wc -l >> ../../count.txt                     
+cd ../../test-3/txt/ && ls -l|wc -l >> ../../count.txt                     
+cd ../../ && cat count.txt    
