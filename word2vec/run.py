fa09c53066c7433ba31ff4edba132a2d66701fc3
orcuslc <orcuslc@hotmail.com>
2016-08-23 00:17:43 +0800

    Modified some scripts for multi-process running

--- word2vec/run.py
+++ word2vec/run.py
@@ -6,28 +6,30 @@ sys.path.insert(0, '..')
 
 from time import time as t
 from nltk_extract.extract_keywords import extract_
-from test import word2vec
+from test import word2vec_
 # from nltk_extract.extract_stem import *
 
-
-method = sys.argv[1]
-sentence_path = sys.argv[2]
-sentence_name = sys.argv[3]
-tmp_path = sys.argv[4]
-result_path = sys.argv[5]
+method = 'sc'
+# method = sys.argv[1]
+# sentence_path = sys.argv[2]
+key_words_path = sys.argv[2:]
+# sentence_name = sys.argv[3]
+sentence_name = key_words_path[-13:]
+# tmp_path = sys.argv[4]
+result_path = sys.argv[1]
 
 def run(sentence_path, sentence_name, result_path):
 	# print sentence_path+sentence_name
 	# t1 = t()
-	extract_(sentence_path+sentence_name, tmp_path+sentence_name)
+	# extract_(sentence_path+sentence_name, tmp_path+sentence_name)
 	# t2 = t()
 	word2vec(method, tmp_path+sentence_name, result_path+sentence_name)
 	# t3 = t()
 	# print t2-t1, t3-t1
 
-def run_(sentence_path, result_path):
-	files = os.listdir(sentence_path)
-	extract_(sentence_path, result_path)
+# def run_(sentence_path, result_path):
+# 	files = os.listdir(sentence_path)
+# 	extract_(sentence_path, result_path)
 
 if __name__ == '__main__':
 	s = t()
--- word2vec/test.py
+++ word2vec/test.py
@@ -59,6 +59,62 @@ def word2vec(method, word_path, result_path):
 	end = time.time()
 	# print(end - start)
 
+
+def word2vec_(method, key_words, result_path):
+	start = time.time()
+	key_word_list = []
+	with open(word_path, 'r') as f:
+		# As the extract_keywords.py would bring another '\n' in the end.
+		words = f.read().split('\n')[:-1]
+		key_word_list.append(words)
+		# print(len(words))
+	f.close()
+	result = []
+
+	if method == 'msc':
+		for word in words:
+			cmd = 'curl http://127.0.0.1:5000/word2vec/msc?word=' + word
+			p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+			r = p.wait()
+			(stdoutput, erroutput) = p.communicate()
+			stdoutput = stdoutput.decode('utf-8').split('\n')[-2]
+			# print(stdoutput.split('\n'))
+			if stdoutput == '0':
+				word = extract(word)
+				cmd = 'curl http://127.0.0.1:5000/word2vec/msc?word=' + word
+				p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+				r = p.wait()
+				(stdoutput, erroutput) = p.communicate()
+				stdoutput = stdoutput.decode('utf-8').split('\n')[-2]
+			result.append(stdoutput)
+	elif method == 'sc':
+		for word in words:
+			cmd = 'curl http://127.0.0.1:5000/word2vec/sc?word=' + word
+			p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+			r = p.wait()
+			(stdoutput, erroutput) = p.communicate()
+			stdoutput = stdoutput.decode('utf-8').split('\n')[-2]
+			# print(stdoutput.split('\n'))
+			if stdoutput == '0':
+				t1 = time.time()
+				word = extract(word)
+				t2 = time.time()
+				print t2-t1
+				cmd = 'curl http://127.0.0.1:5000/word2vec/sc?word=' + word
+				p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+				r = p.wait()
+				(stdoutput, erroutput) = p.communicate()
+				stdoutput = stdoutput.decode('utf-8').split('\n')[-2]
+			result.append(stdoutput)
+	# print(result)
+
+	with open(result_path, 'w') as f:
+		for item in result:
+			f.write(item+'\n')
+	f.close()
+
+	end = time.time()
+
 if __name__ == '__main__':
 	method = sys.argv[1]
 	word_path = sys.argv[2]
